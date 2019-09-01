ebb1e87cd4c4ca19a98af3647191a6ca3d778b5c
leejaehyup <dlwoguq65@naver.com>
2019-09-01 17:47:42 +0900

    'modify'

--- app.js
+++ app.js
@@ -1,119 +1,108 @@
 //var mongojs = require("mongojs");
-var db = null;//mongojs('localhost:27017/myGame', ['account','progress']);
+var db = null; //mongojs('localhost:27017/myGame', ['account','progress']);
 
+require("./Entity");
+require("./client/Inventory");
 
-require('./Entity');
-require('./client/Inventory');
-
-
-var express = require('express');
+var express = require("express");
 var app = express();
-var serv = require('http').Server(app);
+var serv = require("http").Server(app);
 
-app.get('/',function(req, res) {
-	res.sendFile(__dirname + '/client/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/client/index.html");
 });
-app.use('/client',express.static(__dirname + '/client'));
+app.use("/client", express.static(__dirname + "/client"));
 
 serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
 
-
 var DEBUG = true;
 
-var isValidPassword = function(data,cb){
-	return cb(true);
-	/*db.account.find({username:data.username,password:data.password},function(err,res){
+var isValidPassword = function(data, cb) {
+  return cb(true);
+  /*db.account.find({username:data.username,password:data.password},function(err,res){
+		if(res.length > 0)
+			cb(true);
+		else
+			cb(false);
+			
+	});*/
+};
+var isUsernameTaken = function(data, cb) {
+  return cb(false);
+  /*db.account.find({username:data.username},function(err,res){
 		if(res.length > 0)
 			cb(true);
 		else
 			cb(false);
 	});*/
-}
-var isUsernameTaken = function(data,cb){
-	return cb(false);
-	/*db.account.find({username:data.username},function(err,res){
-		if(res.length > 0)
-			cb(true);
-		else
-			cb(false);
-	});*/
-}
-var addUser = function(data,cb){
-	return cb();
-	/*db.account.insert({username:data.username,password:data.password},function(err){
+};
+var addUser = function(data, cb) {
+  return cb();
+  /*db.account.insert({username:data.username,password:data.password},function(err){
 		cb();
 	});*/
-}
+};
 
-var io = require('socket.io')(serv,{});
-io.sockets.on('connection', function(socket){
-	socket.id = Math.random();
-	SOCKET_LIST[socket.id] = socket;
-	
+var io = require("socket.io")(serv, {});
+io.sockets.on("connection", function(socket) {
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;
 
-	socket.on('signIn',function(data){ //{username,password}
-		isValidPassword(data,function(res){
-			if(res){
-				Player.onConnect(socket,data.username);
+  socket.on("signIn", function(data) {
+    //{username,password}
+    isValidPassword(data, function(res) {
+      if (res) {
+        Player.onConnect(socket, data.username);
 
-				socket.emit('signInResponse',{success:true});
-			} else {
-				socket.emit('signInResponse',{success:false});			
-			}
-		});
-	});
-	socket.on('signUp',function(data){
-		isUsernameTaken(data,function(res){
-			if(res){
-				socket.emit('signUpResponse',{success:false});		
-			} else {
-				addUser(data,function(){
-					socket.emit('signUpResponse',{success:true});					
-				});
-			}
-		});		
-	});
-	
-	
-	socket.on('disconnect',function(){
-		delete SOCKET_LIST[socket.id];
-		Player.onDisconnect(socket);
-	});
+        socket.emit("signInResponse", { success: true });
+      } else {
+        socket.emit("signInResponse", { success: false });
+      }
+    });
+  });
+  socket.on("signUp", function(data) {
+    isUsernameTaken(data, function(res) {
+      if (res) {
+        socket.emit("signUpResponse", { success: false });
+      } else {
+        addUser(data, function() {
+          socket.emit("signUpResponse", { success: true });
+        });
+      }
+    });
+  });
 
-	socket.on('sendMsgToServer',function(data){
-		var playerName = ("" + socket.id).slice(2,7);
-		for(var i in SOCKET_LIST){
-			SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
-		}
-	});
+  socket.on("disconnect", function() {
+    delete SOCKET_LIST[socket.id];
+    Player.onDisconnect(socket);
+  });
 
+  socket.on("sendMsgToServer", function(data) {
+    var playerName = ("" + socket.id).slice(2, 7);
+    for (var i in SOCKET_LIST) {
+      SOCKET_LIST[i].emit("addToChat", playerName + ": " + data);
+    }
+  });
 
-	socket.on('evalServer',function(data){
-		if(!DEBUG)
-			return;
-		var res = eval(data);
-		socket.emit('evalAnswer',res);		
-	});
-	
-	
-	
+  socket.on("evalServer", function(data) {
+    if (!DEBUG) return;
+    var res = eval(data);
+    socket.emit("evalAnswer", res);
+  });
 });
 
-
-
-setInterval(function(){
-	var packs = Entity.getFrameUpdateData();
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		socket.emit('init',packs.initPack);
-		socket.emit('update',packs.updatePack);
-		socket.emit('remove',packs.removePack);
-	}
-	
-},1000/25);
+setInterval(function() {
+  var packs = Entity.getFrameUpdateData();
+  for (var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    socket.emit("init", packs.initPack);
+    socket.emit("update", packs.updatePack);
+    socket.emit("remove", packs.removePack);
+  }
+}, 1000 / 25);
 
 /*
 var profiler = require('v8-profiler');
@@ -132,14 +121,3 @@ var startProfiling = function(duration){
 }
 startProfiling(10000);
 */
-
-
-
-
-
-
-
-
-
-
-
